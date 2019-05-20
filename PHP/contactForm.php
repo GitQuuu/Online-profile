fe71f74f0326d5dcebbaf31f2e4435e9f24c52bb
Qu <Quanvle@live.dk>
2019-05-20 13:28:35 +0200

    corrected the action value of the form to the right path
    using link shell extension

--- PHP/contactForm.php
+++ PHP/contactForm.php
@@ -1,5 +1,20 @@
 <?php
-echo "testing backend";
-echo $_POST['name'];
+echo "Message content";
+echo "<br>";
+echo "<br>";
+echo "Name: ". $_POST['Name'];
+echo "<br>";
+echo "Github name: ". $_POST['Github'];
+echo "<br>";
+echo "Email: ". $_POST['Email'];
+echo "<br>";
+echo "Phone number:". $_POST['Phone'];
+echo "<br>";
+echo "<br>";
+echo "Your Message:". $_POST['Message'];
 
+echo "<br>";
+echo "<br>";
+
+echo "You can close this page";
 ?>
