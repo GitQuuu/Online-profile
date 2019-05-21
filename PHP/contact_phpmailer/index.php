c5ddd1022555e95e84cc73a591f3f61c4b3ead00
Qu <Quanvle@live.dk>
2019-05-21 10:03:29 +0200

    Finish the backend on Contactform index.php

--- PHP/contact_phpmailer/index.php
+++ PHP/contact_phpmailer/index.php
@@ -1,3 +1,37 @@
+<?php
+
+$result="";
+if(isset($_POST['submit'])){
+    require 'PHPmailer/PHPMailerAutoload.php';
+
+    // create a mail object
+    $mail = new PHPMailer();
+
+    $mail->Host='smtp.gmail.com';
+    $mail->Port='587';
+    $mail->SMTPAuth=true;
+    $mail->SMTPSecure='tls';
+    $mail->Username='Quan0027.elev.techcollege@gmail.com';
+    $mail->Password='ddn22akd';
+
+    $mail->setFrom($_POST['email'],$_POST['name']);
+    $mail->addAddress('Quanvle@live.dk');
+    $mail->addReplyTo($_POST['email'],$_POST['name']);
+
+    $mail->isHTML(true);
+    $mail->Subject='From online-profile contact form'.$_POST['subject'];
+    $mail->Body='<h1 align=center>Name: '.$_POST['name'].'<br>Email: '. $_POST['email']. '<br>Message: '.$_POST['message'].'</h1>';
+
+    if(!$mail->send()){
+        $result="Something went wrong please try again";
+    }
+    else{
+        $result="Thanks".$_POST['name']."for contacting me, I will get back to you soon";
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <Head>
@@ -27,23 +61,20 @@
         </div>
     </ul>
 </div>
-<p style="font-size:50px; text-align: center">Under construction</p>
+<h2 style="font-size:50px; text-align: center"><?= $result?> construction</h2>
 <div>
-    <form style="float:left; margin-top:20px;" id="Contact" action="/PHP/contactForm.php" method="POST" target="_blank" >
+    <form style="float:left; margin-top:20px;" id="Contact" method="POST"  >
         <label for="name">Input Name:</label><br>
-        <input type="text" name="Name" autofocus/><br>
+        <input type="text" name="name" autofocus/><br>
 
-        <label for="Github">Input Github Name:</label><br>
-        <input type="text" name="Github" placeholder="Optional"/><br>
+        <label for="email">Input email:</label><br>
+        <input type="email" name="email"/><br>
 
-        <label for="Email">Input email:</label><br>
-        <input type="email" name="Email"/><br>
+        <label for="phone">Input phone number:</label><br>
+        <input type="number" name="phone"/><br>
 
-        <label for="Phone">Input phone number:</label><br>
-        <input type="number" name="Phone"/><br>
-
-        <label for="Subject">Input subject:</label><br>
-        <input type="text" name="Subject"/><br>
+        <label for="subject">Input subject:</label><br>
+        <input type="text" name="subject"/><br>
 
         <!--Radio Buttons
             Choose sex:
@@ -63,7 +94,7 @@
                 <input id="Mean" type="checkbox" name="personlity">Mean
         </label><br>-->
 
-        <textarea name="Message" style="width:200px;"placeholder="Message"></textarea><br>
+        <textarea name="message" style="width:200px;"placeholder="Message"></textarea><br>
         <input type="submit" value="SEND" />
         <input type="reset" value="RESET"/>
     </form>
